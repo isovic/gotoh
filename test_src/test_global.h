1eba7b7fb18b4b45ba9427b246fed87d79711948
Ivan Sovic <ivan.sovic@gmail.com>
2016-10-26 15:04:33 +0200

    Testing TravisCI with a failed (on purpose) unit test.

--- test_src/test_global.h
+++ test_src/test_global.h
@@ -84,4 +84,20 @@ TEST(AlignGlobal, CigarComparison5) {
   EXPECT_EQ(cigar_string, std::string("4="));
 }
 
+TEST(AlignGlobal, CigarComparison6) {
+  std::string q("CCGA");
+  std::string t("ACCCGA");
+  is::Penalties p(1, -1, -1, -1);
+  is::AlignType aln_type = is::kGlobal;
+
+  is::Align aln(q, t, p, aln_type, is::GlobalMargins());
+
+  int32_t q_start = 0, q_end = 0, t_start = 0, t_end = 0, score = 0;
+  std::vector<is::CigarOp> cigar;
+  aln.GetAlignment(q_start, q_end, t_start, t_end, score, cigar);
+  std::string cigar_string = CigarToString(cigar);
+
+  EXPECT_EQ(cigar_string, std::string("4X"));
+}
+
 #endif
